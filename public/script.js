f772fc22568467075396919260992516e028809e
Alex <92haal1bif@hft-stuttgart.de>
2020-12-20 13:35:18 +0100

    Added new features

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 $(document).ready((() =>{
     
 
+
 }))
 
 
@@ -9,10 +10,8 @@ function showAlertMessage(showAlertMessage){
     console.log(showAlertMessage)
     const elem = $('#wrong-pw-message')
     if(showAlertMessage == "true"){
-        console.log("bye")
         elem.removeClass('d-none') 
     }else {
-        console.log("hey")
         elem.addClass('d-none')
     }
 
