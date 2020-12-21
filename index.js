3ea7f2559937798d55adde7d65d0cce14034a795
Alex <92haal1bif@hft-stuttgart.de>
2020-12-21 16:59:45 +0100

    Added module

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const passport = require('passport')
 const db = new sqlite3.Database('./db/wack.db');
 
 const port = process.env.PORT || 3000;
